73768417f63a482870d3e8bb70aded678277a11a
Richard White <richard.white@sanger.ac.uk>
2016-01-05 16:28:22 +0000

    Changes to views/targets.tt and lib/CrisprApp.pm views/targets.tt Added div for error message Added form input elements for target (gene id/name), requestor, status
    lib/CrisprApp.pm
    Changes to test objects
    Changed config file to test db
    Added code to /get_targets to fetch targets from db based on form input

--- lib/CrisprApp.pm
+++ lib/CrisprApp.pm
@@ -7,11 +7,11 @@ use English qw( -no_match_vars );
 use Readonly;
 
 my $debug = 1;
-my $test_targets;
+my ( $test_targets, $test_pair_info, );
 if( $debug ){
     $test_targets = [
         {
-            id => 1,
+            target_id => 1,
             target_name => 'test_target_1',
             gene_name => 'test_gene_1',
             gene_id => 'test_gene_id_1',
@@ -21,15 +21,15 @@ if( $debug ){
             status_changed => '2015-12-02',
             crRNAs => [
                 {
-                    id => 1,
-                    crRNA_name => 'crRNA:1:101-123:-1',
+                    crRNA_id => 1,
+                    name => 'crRNA:1:101-123:-1',
                     sequence => 'GGGACATAGACATATAGACGAGG',
                     status => 'FAILED_EMBRYO_SCREENING',
                     status_changed => '2015-12-02',
                 },
                 {
-                    id => 2,
-                    crRNA_name => 'crRNA:1:121-143:1',
+                    crRNA_id => 2,
+                    name => 'crRNA:1:121-143:1',
                     sequence => 'GGTACGATATATATGCAACGAGG',
                     status => 'PASSED_EMBRYO_SCREENING',
                     status_changed => '2015-12-02',
@@ -37,7 +37,7 @@ if( $debug ){
             ]
         },
         {
-            id => 2,
+            target_id => 2,
             target_name => 'test_target_2',
             gene_name => 'test_gene_2',
             gene_id => 'test_gene_id_2',
@@ -47,25 +47,59 @@ if( $debug ){
             status_changed => '2015-12-02',
         },
     ];
+    
+    $test_pair_info = [
+        {
+            pair_name => '19:10678178-10679198:1',
+            primer_pair_id => 1,
+            left_primer => {
+                sequence => 'ACGATAGCATATAGACGAATAGG',
+                plate_name => 'CR_000026h',
+                well_id => 'A01',
+            },
+            right_primer => {
+                sequence => 'TGATGAGCATACTGCACGATATTAG',
+                plate_name => 'CR_000026h',
+                well_id => 'A01',
+            },
+        },
+        {
+            pair_name => '25:1-250:1',
+            primer_pair_id => 2,
+            left_primer => {
+                sequence => 'GACGATGACGATAGATGACGA',
+                plate_name => 'CR_000026h',
+                well_id => 'B01',
+            },
+            right_primer => {
+                sequence => 'TGATGAGCATACTGCACGATATTAG',
+                plate_name => 'CR_000026h',
+                well_id => 'B01',
+            },
+        },
+    ];
 }
-# else{
-#     use Crispr;
-#     use Crispr::Target;
-#     use Crispr::Config;
-#     use Crispr::DB::DBConnection;
-#     use Crispr::DB::TargetAdaptor;
-# }
+else{
+    use Crispr;
+    use Crispr::Target;
+    use Crispr::Config;
+    use Crispr::DB::DBConnection;
+    use Crispr::DB::TargetAdaptor;
+}
 
 our $VERSION = '0.1';
 
-Readonly my $crispr_db = 'zmp_crispr_db.conf';
+Readonly my $crispr_db => '/nfs/users/nfs_r/rw4/config/rw4_crispr_test.conf';
 
-my $target_adaptor;
+my ( $target_adaptor, $crRNA_adaptor, $primer_pair_adaptor, $plate_adaptor );
 if( !$debug ){
     # connect to db
     my $DB_connection = Crispr::DB::DBConnection->new( $crispr_db );
     # get adaptors
     $target_adaptor = $DB_connection->get_adaptor( 'target' );
+    $crRNA_adaptor = $DB_connection->get_adaptor( 'crRNA' );
+    $primer_pair_adaptor = $DB_connection->get_adaptor( 'primer_pair' );
+    $plate_adaptor = $DB_connection->get_adaptor( 'plate' );
 }
 
 hook before_template => sub {
@@ -74,50 +108,61 @@ hook before_template => sub {
     $tokens->{'home_url'} = uri_for('/');
     $tokens->{'targets_url'} = uri_for('/targets');
     $tokens->{'sgrnas_url'} = uri_for('/sgrnas');
-    $tokens->{'primers_url'} = uri_for('/primers');
+    $tokens->{'primers_url'} = uri_for('/primer_pairs');
     $tokens->{'miseq_url'} = uri_for('/miseq');
 };
 
 get '/' => sub {
     template 'index', {
         template_name => 'home',
-        test_text => 'This is some test text!',
     };
 };
 
 get '/targets' => sub {
     template 'targets', {
         template_name => 'targets',
-        test_text => 'Target test text.',
         get_targets_url => uri_for('/get_targets'),
     };
 };
 
 get '/get_targets' => sub {
     my $targets;
-    debug param('gene_name');
+    my $target_info = param('target');
+    my $requestor = param('requestor');
+    my $status = param('status');
     if( $debug ){
         $targets = $test_targets;
     }
     else{
-        $targets = $target_adaptor->fetch_all_by_gene_name( param('gene_name') );
+        if( $target_info ){
+            $targets = $target_adaptor->fetch_all_by_target_name_gene_id_gene_name( $target_info );
+        }
+        elsif( $requestor ){
+            $targets = $target_adaptor->fetch_all_by_requestor( $requestor );
+        }
+        elsif( $status ){
+            $targets = $target_adaptor->fetch_all_by_status( $status );
+        }
     }
-    my $err_msg = join(q{ }, "Couldn't find a match for", param('gene_name') );
+    my $err_msg;
     if( scalar @{$targets} == 0 ){
+        $err_msg = "Couldn't find any matches for:<br>";
+        $err_msg .= join(' - ', 'Target', $target_info, ) . '<br>' if $target_info;
+        $err_msg .= join(' - ', 'Requestor', $requestor, ) . '<br>' if $requestor;
+        $err_msg .= join(' - ', 'Status', $status, ) . '<br>' if $status;
+    }    
+    
+    if( $err_msg ){
         template 'targets', {
-            template_name => 'targets',
             err_msg => $err_msg,
-            targets => $targets,
-            target_url => uri_for('/target'),
+            target_url => uri_for('/get_targets'),
         };
     }
     else{
         template 'show_targets', {
-            template_name => 'show_targets',
-            test_text => 'Show Targets test text.',
             targets => $targets,
             target_url => uri_for('/target'),
-        };
+        };        
     }
 };
 
